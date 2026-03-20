932dfc036715116a2899b72ad67296d2d68de1f4
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-20 19:43:07 +0000

    feat: implement core structure of AuthSection component

--- src/Components/AuthSection/AuthSection.jsx
+++ src/Components/AuthSection/AuthSection.jsx
@@ -1,12 +1,48 @@
 import "./AuthSection.css";
 
-import user from "~/Assets/user.png";
+import userIco from "~/Assets/user.png";
+import emailIco from "~/Assets/email.png";
+import passwordIco from "~/Assets/password.png";
 
 
 let AuthSection = () => {
     return (
-        <div>
+        <div className="container">
 
+            <div className="header-section">
+                <p className="header-text">Sign Up</p>
+                <div className="underline"></div>
+            </div>
+
+            <div className="input-section">
+
+                <div className="input-item">
+                    <img className="icon" src={userIco} alt="username" />
+                    <input type="text" name="" id="" />
+                </div>
+
+                <div className="input-item">
+                    <img className="icon" src={emailIco} alt="email" />
+                    <input type="email" name="" id="" />
+                </div>
+
+                <div className="input-item">
+                    <img className="icon" src={passwordIco} alt="password" />
+                    <input type="password" name="" id="" />
+                </div>
+
+            </div>
+
+            <div className="button-section">
+                <button className="sign-in-btn">SIGN IN</button>
+                <button className="sign-up-btn">SIGN UP</button>
+            </div>
+
+            <div className="forgot-password">
+                Forgot your password?
+                <a href="">CLICK HERE</a>
+            </div>
+            
         </div>
     )
 };
