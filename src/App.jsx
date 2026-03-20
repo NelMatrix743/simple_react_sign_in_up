9c760bfd1e4faa28fa359a56458c386f06daa9a4
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-20 10:59:35 +0000

    genesis: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+
+let App = () => {
+  return (
+    <main className="main-section">
+
+      {/* implementation starts here */}
+      
+
+    </main>
+  )
+};
+
+
+export default App;
